22bb0ea64fc5dffa024c70daf035b1f91f45068b
Cynthia <Cynthia@10.173.110.139>
2017-03-15 09:51:40 +0000

    Added GuiTest

---
+++ test/EchoGUITest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cynthia
+ */
+public class EchoGUITest {
+    
+    public EchoGUITest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of addButtons method, of class EchoGUI.
+     */
+    @Test
+    public void testAddButtons() {
+        System.out.println("addButtons");
+        EchoGUI instance = null;
+        instance.addButtons();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of changeColor method, of class EchoGUI.
+     */
+    @Test
+    public void testChangeColor() {
+        System.out.println("changeColor");
+        String color = "";
+        EchoGUI instance = null;
+        instance.changeColor(color);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of Flash method, of class EchoGUI.
+     */
+    @Test
+    public void testFlash() {
+        System.out.println("Flash");
+        EchoGUI instance = null;
+        instance.Flash();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
