2e0c27b50aa985a5b59138da19e529a69f9a420c
lrcd201 <lrcd201@exeter.ac.uk>
2017-03-21 15:13:58 +0000

    ComputationalTest fully commented, implemented etc.

--- test/ComputationalTest.java
+++ test/ComputationalTest.java
@@ -1,3 +1,5 @@
+// SHOULD BE FINISHED
+
 import java.io.*;
 import java.net.URLEncoder;
 import org.junit.After;
@@ -8,7 +10,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * The test class for Computational. Contains three tests:
+ * 
+ * 1. Tests that the solve method works as intended
+ * 2. Tests that the urlEncode method works as intended
+ * 3. Tests that the getAnswer method works as intended
  */
 public class ComputationalTest {
     
@@ -31,38 +37,48 @@ public class ComputationalTest {
     public void tearDown() {
     }
     
+    /**
+     * Test to check that the solve method works as intended
+     */
     @Test
     public void testSolve() throws IOException{
-        Computational c = new Computational();
+        Computational c = new Computational(); // Instance to test created
+        
         String resp = c.solve("What is the melting point of silver?");
-        //resp will be a JSON file
-        assertNotNull("resp should not be null", resp);        
-        }
-    
-    @Test
-    public void testSolve2() throws IOException{
-        Computational c = new Computational();
-        String resp = c.solve("asdfghjkl;");
-        if (resp.contains("success=\'false\'")){
-            assertNotNull("resp shouldn't be null", resp);
+        assertNotNull("resp shouldn't be null", resp);
+
+        Computational c1 = new Computational(); // Instance to test created
+        
+        String resp1 = c1.solve("asdfghjkl;");
+        if (resp1.contains("success=\'false\'")){
+            assertNotNull("resp shouldn't be null", resp1);        
         }
     }
-
+    
+    /**
+     * Test to check that the urlEncode method works as intended
+     */
     @Test
     public void testUrlEncode(){
-        Computational c = new Computational();
+        Computational c2 = new Computational(); // Instance to test created
+        // Variable defined to test the method
         String question = "What is the melting point of silver?";
-        String resp = c.urlEncode(question);
-        assertEquals("What+is+the+melting+point+of+silver%3F", resp);
+        
+        String resp2 = c2.urlEncode(question);
+        assertEquals("What+is+the+melting+point+of+silver%3F", resp2);
     }
     
+    /**
+     * Test to check that the getAnswer method works as intended
+     */
     @Test
     public void testGetAnswer(){
-        Computational c = new Computational();
+        Computational c3 = new Computational(); // Instance to test created
+        // Variable defined to test the method
         String question = "What is the melting point of silver?";
-        String resp = c.getAnswer(question);
-        assertEquals("961.78 C  (degrees Celsius)", resp);
         
+        String resp3 = c3.getAnswer(question);
+        assertEquals("961.78 C  (degrees Celsius)", resp3);       
     }
 }
     
